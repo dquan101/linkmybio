bdf4ab6eb0146068f60c54e592ada3fdf346a057
Diego Quan <diegoquan101@gmail.com>
2019-10-01 07:34:26 -0600

    Initial Flask

---
+++ bio.py
@@ -0,0 +1,18 @@
+from Flask import Flask, jsonify, render_template, request
+import sys, yaml, os
+
+app =  Flask(__name__)
+environment = os.getenv("ENVIRONMENT", "development")
+
+with open('links.yaml') as f:
+    links = yaml.load(f, Loader=yaml.FullLoader)
+
+@app.route("/home")
+def home():
+    pass
+
+if __name__ == "__main__":
+    debug=True
+    if environment == 'development':
+        debug = True
+    app.run(host="0.0.0.0", debug=debug)
